7f721deb5f1c56032426b5a459f82ef823e7ffcd
sharifullah shirzad <sharifullahshirzad81@gmail.com>
2022-09-26 11:59:21 +0430

    my file

---
+++ jqueryshirzad.js
@@ -0,0 +1,40 @@
+
+// $('#p1').click(()=>{
+//     $('#theball').animate({left: '900px'},2000)
+// })
+
+// $('#p2').click(()=>{
+//     $('#theball').animate({left: '0px'},3000)
+// })
+
+
+
+
+
+//remove elements
+
+ $('button').click(()=>{
+// //     // $('p').remove('.test, #test');
+
+// //     // $('div').empty();
+
+//     // $('#test, .test').addClass('parnt fontsize')
+
+//     // $('div, ul, li, span').toggleClass('pressed')
+
+// //     $('span').removeClass('pressed')
+
+//         $('div').css({'color':"gray",'font-size':'40px','background-color':'gold'})
+
+//         $('button').css('background-color',"").addClass('btn btn-info')
+
+        // $('h1').clone().appendto('span');
+
+        // $('small').wrapInner('<mark></mark>')
+        $('div').children('p:first').css({'border':'2px solid red', 'color':'red'})
+
+ })
+
+
+
+
